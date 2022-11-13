c9f2d7b44f010230786f0cd4135f66254f9a12f6
Umar-37 <omaralhussan@outlook.com>
2022-11-13 17:38:04 +0300

    add hashing

--- src/models/mythical_weapon.ts
+++ src/models/mythical_weapon.ts
@@ -1,4 +1,6 @@
+import { response } from "express";
 import client from "../database";
+import bcrypt from "bcrypt"
 
 export type Weapon = {
     id?: number;
@@ -6,6 +8,7 @@ export type Weapon = {
     type: String;
     weight: Number
 }
+
 
 export class MythicalWeaponStore {
     async index(): Promise<Weapon[]> {
@@ -21,7 +24,7 @@ export class MythicalWeaponStore {
 
         }
     }
-    async show(id: string): Promise<Weapon[]> {
+    async show(id: string): Promise<Weapon> {
         try {
             const conn = await client.connect()
             const sql = `SELECT * FROM mythical_weapons WHERE id=${id}`
@@ -38,13 +41,24 @@ export class MythicalWeaponStore {
             console.log("in create", weapon);
             const conn = await client.connect()
             const sql = 'INSERT INTO mythical_weapons(name, type, weight) VALUES ($1, $2, $3) RETURNING *'
+            const pepper:string=process.env.BCRYPT_PASSWORD as string
+            const saltRounds:string=process.env.SALT_ROUNDS as string
+            const hash=bcrypt.hashSync(weapon.name +pepper,parseInt(saltRounds))
 
         try {
             // const sql = `INSERT INTO mythical_eapons(name, type, weight) VALUES ('omar', 'water',99 );`
             //const sql = `INSERT INTO mythical_weapons(name, type, weight) VALUES (${weapon.name}, ${weapon.type}, ${weapon.weight});`
             //const result = await conn.query(sql)
 
-        let result =  await conn.query(sql, [weapon.name, weapon.type, weapon.weight])
+        let result =  await conn.query(sql, [hash, weapon.type, weapon.weight]
+           //debug this
+        //     ,(err:any,res:any)=>{
+        //     if(err) console.log('Error happen:',err);
+
+        //     return res;
+            
+        // }
+        )
             //console.log('reult is:', result);
 
             conn.release()
@@ -53,6 +67,25 @@ export class MythicalWeaponStore {
         } catch (err) {
             throw new Error(`error happen :${err}`);
 
+        }
+    }
+    async remove(id: string): Promise<Weapon> {
+            const conn = await client.connect()
+        try {
+            const deleted =this.show(id)
+            const conn = await client.connect()
+            const sql = `DELETE FROM mythical_weapons WHERE id=${id}`
+            const result = await conn.query(sql)
+            console.log('result is :',result);
+            
+            //return result.rows
+            return deleted 
+        } catch (err) {
+            throw new Error(`error happen :${err}`);
+
+        }finally{
+
+            conn.release()
         }
     }
 
